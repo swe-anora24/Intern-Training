7f1899cf571ccadc537812ed8630f2cd7a755fd3
AIPLBLRNT0018\Chethan Prasad S <chethanprasad.s@anorasolutions.com>
2025-04-21 14:24:57 +0530

    Pointer check program is added

---
+++ Chethan/PointerCheck.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+
+typedef struct company{
+    int x;
+    int y;
+    char c;
+}comp1;
+
+int main(){
+    comp1 comp2;
+     comp1 *ptr=(comp1 *)(malloc(sizeof(comp2)));
+
+     ptr->x=10;
+     ptr->y=20;
+     ptr->c='H';
+
+     printf("%d\n",ptr->x);
+     printf("%d\n",ptr->y);
+     printf("%c\n",ptr->c);
+
+     free(ptr);
+
+     printf("%d\n",ptr->x);
+
+}
