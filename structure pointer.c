1b92b7369a29d42d21d35df064e563a3c6588108
AIPLBLRNT0018\Chethan Prasad S <chethanprasad.s@anorasolutions.com>
2025-04-23 10:28:14 +0530

    The main

---
+++ structure pointer.c
@@ -0,0 +1,27 @@
+/* BY using typedef */
+#include <stdio.h>
+#include<string.h>
+
+typedef struct company{
+    int x;
+    int y;
+    char c;
+}comp1;  
+
+int main(){
+     comp1 *ptr=(comp1 *)malloc(sizeof(comp1));
+
+     ptr->x=10;
+     ptr->y=20;
+     ptr->c='H';
+
+     printf("%d\n",ptr->x);
+     printf("%d\n",ptr->y);
+     printf("%c\n",ptr->c);
+
+     free(ptr);
+
+     printf("%d\n",ptr->x);
+     printf("%d\n",ptr->y);
+     printf("%c",ptr->c);
+}
