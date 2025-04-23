671e1fe4ca67f33084795fcb18dacf97014bc2b4
AIPLBLRNT0018\Chethan Prasad S <chethanprasad.s@anorasolutions.com>
2025-04-23 17:09:25 +0530

    Everything is fine

--- Check for stashes.c
+++ Check for stashes.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main(){
-    printf("The main ");  
+    printf("The main ");
+    printf("Now for stashing");
+    printf("Extra line for stashes");
+    printf("Once ")
+    printf("The branch after stashes");
 }
-
